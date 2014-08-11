2a7681d1cac0154084d409888574d393a1390ff3
John Cashmore <cashy@whoooop.co.uk>
2014-08-11 14:44:43 +0100

    changes

--- bin/cli.js
+++ bin/cli.js
@@ -4,5 +4,16 @@ var fontsUsed = require("../index.js");
 var read = require('fs').readFileSync;
 
 var css = read(process.argv[2], 'utf8').toString();
+var fonts = fontsUsed.findFonts(css);
 
-console.log(fontsUsed.findFonts(css));
+if(fonts.length > 0) {
+	console.log('Fonts Found');
+	console.log('=============');
+
+	fonts.forEach(function (font) {
+
+		console.log(font.fontName + ' - ' + font.occurances);
+	});
+} else {
+	console.log('no fonts founds')
+}
